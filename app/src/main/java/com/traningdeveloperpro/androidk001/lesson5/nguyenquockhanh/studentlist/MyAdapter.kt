db3bc45b5a97c1c8259ccc1c7d26cd39ba2f0d4a
Nguyen Quoc Khanh <nguyenkhanhcv@gmail.com>
2019-07-31 20:52:28 +0700

    Intial commit

---
+++ app/src/main/java/com/traningdeveloperpro/androidk001/lesson5/nguyenquockhanh/studentlist/MyAdapter.kt
@@ -0,0 +1,29 @@
+package com.traningdeveloperpro.androidk001.lesson5.nguyenquockhanh.studentlist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private val items: ArrayList<Student>) : RecyclerView.Adapter<StudentViewHolder>() {
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return StudentViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        val student: Student = items[position]
+        holder.bind(student)
+        holder.buttonDelete?.setOnClickListener {
+            val itemLabel = items[position]
+            items.removeAt(position)
+            notifyItemRemoved(position)
+            notifyItemRangeChanged(position, items.size)
+        }
+    }
+
+
+}
